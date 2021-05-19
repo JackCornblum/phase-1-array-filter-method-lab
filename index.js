c933e588a54cbcf33036d81ae4b660d038dcccb8
jackcornblum <jcornblum@gmail.com>
2021-05-19 13:55:45 -0400

    commit tree 9401c8ecedb9c29dada51e8b4aa3cafae048b453

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter(function(person){return person.toLowerCase() === name.toLowerCase()});
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, name){
+    //returns if the first letter of the name matches for both
+    const newDrivers = drivers.filter(function(person){return person[0] === name[0]});
+    return newDrivers;
+}
+
+function matchName(driver, driverName) {
+    const newDrivers = driver.filter(function(person){return person.name === driverName});
+    
+    return newDrivers;
+}
